90ac3cddd0cfacfa41d534f336b0daa4dcc1c96b
mfd713 <friendmadeline@gmail.com>
2020-04-18 16:45:00 -0500

    coded first two activities

---
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -0,0 +1,6 @@
+let product = 9*9;
+let quotient = 56/12;
+let remainder = 281%9;
+let inf = 18/0;
+let stringSum = "56" + "92";
+let comparison = 42===52;
---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,6 @@
+let input = prompt("What's your name?");
+if (input.length > 4){
+    alert("Your name is more than four characters!");
+}
+else
+    alert("You name is less than four characters!")
