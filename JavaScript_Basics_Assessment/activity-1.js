b4919ad7ac9fd6e03cf38e596471ab802c076db4
mfd713 <friendmadeline@gmail.com>
2020-04-19 13:11:04 -0500

    removed variable declarations

--- JavaScript_Basics_Assessment/activity-1.js
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -1,6 +1,6 @@
-let product = 9*9;
-let quotient = 56/12;
-let remainder = 281%9;
-let inf = 18/0;
-let stringSum = "56" + "92";
-let comparison = 42===52;
+9*9;
+56/12;
+281%9;
+18/0;
+"56" + "92";
+42===52;
